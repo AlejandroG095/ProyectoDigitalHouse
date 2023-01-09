b8c6f79344359aa2c517f723551965946d8d0ec6
Alejandro Gutierrez <alejoguti77@gmail.com>
2023-01-09 18:29:31 -0500

    cambios aplicados a los comandos de la base de datos

--- Mysql project database/DbBackup.sql
+++ Mysql project database/DbBackup.sql
@@ -19,6 +19,9 @@
 -- Table structure for table `users`
 --
 
+CREATE DATABASE IF NOT EXISTS project_db;
+USE project_db;
+
 DROP TABLE IF EXISTS `users`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
